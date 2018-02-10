5649ea4cbd2309d21a4044f3efd915225e594ed3
Jay Aichelman <jay.aichelman@gmail.com>
2018-02-10 14:00:41 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router'
 
+const appRoutes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'team', component: TeamComponent},
+  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TeamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/strength.model.ts
@@ -0,0 +1,4 @@
+export class Strength {
+  strengthRating: string;
+  message: string;
+}
